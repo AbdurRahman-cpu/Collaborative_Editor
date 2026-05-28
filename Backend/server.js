d32b5d663e750fbb74d10cc688a939e7eec208fd
AbdurRahman-cpu <abdurrahmanmohammed043@gmail.com>
2026-05-28 20:31:44 +0530

    Added public folder to backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ import {YSocketIO} from 'y-socket.io/dist/server'
 import * as http from "node:http";
 
 const app = express()
+
+app.use(express.static('public'))
 const httpServer = createServer(app)
 
 
@@ -19,12 +21,12 @@ const ySocketIO = new YSocketIO(io)
 ySocketIO.initialize()
 
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello World!',
-        success : true
-    })
-})
+// app.get('/', (req, res) => {
+//     res.status(200).json({
+//         message: 'Hello World!',
+//         success : true
+//     })
+// })
 app.get('/health', (req, res) => {
     res.status(200).json({
         message: 'OK',
--- Frontend/src/App/App.jsx
+++ Frontend/src/App/App.jsx
@@ -1,33 +1,54 @@
-import './App.css'
-import Editor from '@monaco-editor/react'
-import {MonacoBinding} from 'y-monaco'
-import {useRef,useMemo,useState,useEffect} from "react"
-import * as Y from  'yjs'
-import {SocketIOProvider} from "y-socket.io"
+import "./App.css"
+import { Editor } from "@monaco-editor/react"
+import { MonacoBinding } from "y-monaco"
+import { useRef, useMemo, useState, useEffect } from "react"
+import * as Y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
 
 function App() {
 
-    const [UserName, setUserName] = useState(()=>{
-        return new URLSearchParams(window.location.search).get('userName') || ""
-    })
-    const [users, setUsers] = useState([])
-
     const editorRef = useRef(null)
+    const [ username, setUsername ] = useState(() => {
+        return new URLSearchParams(window.location.search).get("username") || ""
+    })
+    const [ users, setUsers ] = useState([])
 
-    const ydoc = useMemo(()=> new Y.Doc(),[])
-    const ytext = useMemo(()=>ydoc.getText('monaco'),[ydoc])
+    const ydoc = useMemo(() => new Y.Doc(), [])
+    const yText = useMemo(() => ydoc.getText("monaco"), [ ydoc ])
+
 
     const handleMount = (editor) => {
         editorRef.current = editor
+
+        new MonacoBinding(
+            yText,
+            editorRef.current.getModel(),
+            new Set([ editorRef.current ]),
+        )
+    }
+
+
+    const handleJoin = (e) => {
+        e.preventDefault()
+        setUsername(e.target.username.value)
+        window.history.pushState({}, "", "?username=" + e.target.username.value)
+
+
+
     }
 
     useEffect(() => {
-        if(UserName && editorRef.current){
-            const provider = new SocketIOProvider("http://localhost:8080","monaco",ydoc,{
-                autoConnect: true,
-            }) // connects frontend editor to server
 
-            provider.awareness.setLocalSteteField('user',{UserName})
+        console.log(username)
+
+        if (username) {
+
+            const provider = new SocketIOProvider("/", "monaco", ydoc, {
+                autoConnect: true,
+            })
+
+            provider.awareness.setLocalStateField("user", { username })
+
 
             const states = Array.from(provider.awareness.getStates().values())
 
@@ -35,7 +56,7 @@ function App() {
 
             setUsers(states.filter(state => state.user && state.user.username).map(state => state.user))
 
-            provider.awareness.on("change",()=>{
+            provider.awareness.on("change", () => {
                 const states = Array.from(provider.awareness.getStates().values())
                 setUsers(states.filter(state => state.user && state.user.username).map(state => state.user))
             })
@@ -46,68 +67,67 @@ function App() {
 
             window.addEventListener("beforeunload", handleBeforeUnload)
 
-            const monacoBinding = new MonacoBinding(
-                ytext,
-                editorRef.current.getModel(),
-                new Set([editorRef.current]),
-                provider.awareness,
-            )
+
             return () => {
                 provider.disconnect()
                 window.removeEventListener("beforeunload", handleBeforeUnload)
             }
         }
-    },[editorRef.current])
+    }, [
+        username
+    ])
 
-    const handleSave = (e) => {
-        setUserName(e.target.userName.value)
-        e.preventDefault()
-        window.history.pushState({},"","?userName="+e.target.userName.value)
-    }
-
-    if(!UserName){
-        return(
-            <>
-                <main className='h-screen w-full bg-black flex justify-center items-center '>
-                        <form
-                            className='flex flex-col gap-2  rounded-lg  '
-                            onSubmit={ handleSave }
-                        >
-                            <input type='text'
-                                   placeholder='Name'
-                                   className='bg-gray-800 p-2 rounded-2xl text-white'
-                                   name='userName'
-                            />
-                            <button type='submit' className='bg-amber-100 text-gray-500 font-bold rounded-2xl'>Join</button>
-                        </form>
-                </main>
-            </>
+    if (!username) {
+        return (
+            <main className="h-screen w-full bg-gray-950 flex gap-4 p-4 items-center justify-center" >
+                <form
+                    onSubmit={handleJoin}
+                    className="flex flex-col gap-4">
+                    <input
+                        type="text"
+                        placeholder="Enter your username"
+                        className="p-2 rounded-lg bg-gray-800 text-white"
+                        name="username"
+                    />
+                    <button
+                        className="p-2 rounded-lg bg-amber-50 text-gray-950 font-bold"
+                    >
+                        Join
+                    </button>
+                </form>
+            </main>
         )
     }
 
+    return (
+        <main
+            className="h-screen w-full bg-gray-950 flex gap-4 p-4"
+        >
+            <aside
+                className="h-full w-1/4 bg-amber-50 rounded-lg "
+            >
+                <h2 className="text-2xl font-bold p-4 border-b border-gray-300">Users</h2>
+                <ul className="p-4">
+                    {users.map((user, index) => (
+                        <li key={index} className="p-2 bg-gray-800 text-white rounded mb-2">
+                            {user.username}
+                        </li>
+                    ))}
+                </ul>
 
+            </aside>
+            <section
+                className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+                <Editor
+                    height="100%"
+                    defaultLanguage="javascript"
+                    defaultValue="// some comment"
+                    theme="vs-dark"
+                    onMount={handleMount}
+                />
+            </section>
 
-    return(
-        <>
-            <main className='h-screen w-screen bg-black flex gap-2 p-2'>
-                <aside
-                    className="h-full w-1/4 bg-amber-50 rounded-lg "
-                >
-                    <h2 className="text-2xl font-bold p-4 border-b border-gray-300">Users</h2>
-                    <ul className="p-4">
-                        {users.map((user, index) => (
-                            <li key={index} className="p-2 bg-gray-800 text-white rounded mb-2">
-                                {user.username}
-                            </li>
-                        ))}
-                    </ul>
-
-                </aside>
-                <section className='h-full w-4/5 bg-gray-400 rounded-2xl overflow-hidden    '>
-                    <Editor height="90vh" defaultLanguage="javascript" defaultValue="//️HI" onMount={handleMount} />
-                </section>
-            </main>
-        </>
+        </main>
     )
 }
 
